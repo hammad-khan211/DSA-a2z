6b2cf547aae300620f041454e1e645ac06cbc8fc
Ahmad Hammad Khan <ahmadhammadkhan21112005@gmail.com>
2026-07-27 12:38:48 +0530

    BS on answers

---
+++ Arrays/searching/Binary Search/BS on Answers/FindSQRT.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class FindSQRT
+{
+    static int findSquareRoot(int x)
+    {
+        if(x == 0) return 0;
+        long low = 1;
+        long high = (x / 2) + 1;
+        while(low <= high)
+        {
+            long mid = low + ((high - low) / 2);
+            long p = mid * mid;
+            if(p <= x)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return (int)high;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+        int ans = findSquareRoot(n);
+        System.out.println("The floor square root of " + n + " is : " + ans);
+        sc.close();
+
+    }
+}
